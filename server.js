fcc24942bedd8a86fceda5ec6ceb2f3a5d8610c4
amitgiti137 <amit1317jmp@gmail.com>
2025-01-29 13:00:08 +0530

    Fix Vercel deployment for Express

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/task'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+/* const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); */
+module.exports = app;
