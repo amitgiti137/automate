d762bd5154ce26347b1e7ef5f47ba3469af2064b
amitgiti137 <amit1317jmp@gmail.com>
2025-02-06 15:46:35 +0530

    Initial commit

--- models/User.js
+++ models/User.js
@@ -43,12 +43,12 @@ UserSchema.pre('validate', async function (next) {
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
+/* UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-});
+}); */
 
 if (!global.UserModel) {
     global.UserModel = mongoose.models.User || mongoose.model('User', UserSchema);
